7a0e0b2b17045a3d79529afca8c81b220d1e50e9
Peter Loos <cpp@peterloos.de>
2025-10-20 09:27:44 +0200

    Add project files.

---
+++ Clean_Performant_Code/Data_Structures_and_Algorithms/Program.cpp
@@ -0,0 +1,20 @@
+// ===========================================================================
+// Program.cpp // Data Structures and Algorithms
+// ===========================================================================
+
+extern void test_stl_container();
+extern void test_algorithms();
+extern void test_hashing();
+
+int main()
+{
+    test_stl_container();
+    test_algorithms();
+    test_hashing();
+
+    return 0;
+}
+
+// ===========================================================================
+// End-of-File
+// ===========================================================================
