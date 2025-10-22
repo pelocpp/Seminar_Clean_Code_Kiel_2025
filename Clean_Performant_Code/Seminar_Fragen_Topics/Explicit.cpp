35162fc06ca23e4e797491802f5f32de30487d57
Peter Loos <cpp@peterloos.de>
2025-10-22 16:56:18 +0200

    Stand Mittwoch Abend.

---
+++ Clean_Performant_Code/Seminar_Fragen_Topics/Explicit.cpp
@@ -0,0 +1,66 @@
+// =====================================================================================
+// Explicit.cpp // Keyword 'explicit'
+// =====================================================================================
+
+
+#include <cstdint>   // for uint8_t
+#include <iostream>
+#include <iomanip>
+#include <array>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <print>
+#include <numeric>
+
+
+namespace KeywordExplicit {
+
+    class Complex {
+    private:
+        double m_real;
+        double m_imag;
+
+    public:
+        // c'tors
+        Complex() : Complex{ 0.0, 0.0 } {}
+
+        explicit  Complex(double real) :   // remove or add keyword 'explicit'
+            m_real{ real }, m_imag{} {
+        }
+
+        Complex(double real, double imag) :
+            m_real{ real }, m_imag{ imag } {
+        }
+
+        // comparison operator
+        bool operator == (Complex rhs)
+        {
+            return (m_real == rhs.m_real && m_imag == rhs.m_imag);
+        }
+    };
+}
+
+void main_explicit_keyword()
+{
+    using namespace KeywordExplicit;
+
+    // a Complex object
+    Complex c{ 3.0, 0.0 };
+
+
+
+    if (c == Complex (3.0))                            // remove or add cast '(Complex)'
+    {
+        std::cout << "Same objects" << std::endl;
+    }
+    else
+    {
+        std::cout << "Not same objects" << std::endl;
+    }
+}
+
+// =====================================================================================
+// End-of-File
+// =====================================================================================
