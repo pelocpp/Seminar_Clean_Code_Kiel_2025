9c3f4afadda582c2bbef3190e87985a64266d3a5
Peter Loos <cpp@peterloos.de>
2025-10-22 18:40:52 +0200

    Sources und Text aufgeräumt

--- Clean_Performant_Code/Seminar_Fragen_Topics/Program.cpp
+++ Clean_Performant_Code/Seminar_Fragen_Topics/Program.cpp
@@ -1,10 +1,7 @@
 extern void main_lambdas();
-
 extern void main_constexpr();
-
 extern void main_literals();
 extern void main_brace_initialization();
-
 extern void main_initializer_list();
 extern void main_explicit_keyword();
 
